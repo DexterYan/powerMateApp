f481c14b54e89aabdf96cb02609e4d8550abbca4
Dexter Yan <yanshaocong@gmail.com>
2015-08-09 18:31:09 +0800

    add alert to config

--- www/js/controllers/DIYController.js
+++ www/js/controllers/DIYController.js
@@ -1,6 +1,19 @@
 angular.module('diy.controller', ['starter.services'])
 
 
-.controller('DIYCtrl', function($scope, socket) {
-    
+.controller('DIYCtrl', function($scope, $ionicTabsDelegate, SweetAlert) {
+    SweetAlert.swal({
+       title: "How many keypads do you need?",
+       html: true,
+       type: "input",
+       inputType: "number",
+       inputPlaceholder: "Input a number, from 1 to 11",
+       showCancelButton: true,
+       confirmButtonColor: "#DD6B55",
+       confirmButtonText: "Next",
+       closeOnConfirm: false},
+        function(inputValue){
+            $scope.totalNumber = inputValue;
+            SweetAlert.swal("Nice!", "You wrote: " + inputValue, "success");
+        })
 })
