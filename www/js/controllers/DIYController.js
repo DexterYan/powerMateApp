bdcc50552b10385bbfd84f80fa63ddb47fcb46f3
Dexter Yan <yanshaocong@gmail.com>
2015-08-15 17:57:30 +0800

    add set total keypad number and every keypad style

--- www/js/controllers/DIYController.js
+++ www/js/controllers/DIYController.js
@@ -1,19 +1,63 @@
 angular.module('diy.controller', ['starter.services'])
 
+.controller('DIYCtrl', function($scope, $ionicTabsDelegate, ngDialog) {
+     var keypadName = ['First', 'Second', 'Third', 'Fourth', 
+                                        'Fifth', 'Sixth', 'Seventh', 'Eighth'];
+    $scope.keypadsTypes = [];
+    $scope.configStepTwo = function(keypadNumber) {
+        var keypadNumberTmp = keypadNumber - 1;
+        ngDialog.open({
+            template: 'stepTwo',
+            closeByDocument: false,
+            scope: $scope,
+            controller: ['$scope', function($scope) {
+                $scope.keypadName = keypadName[keypadNumberTmp];
+                $scope.confirm = function(valName, val) {
+                    $scope.$parent.keypadsTypes.push({
+                        name: keypadName[keypadNumberTmp], 
+                        type: val, 
+                        index: keypadNumberTmp
+                    })
+                    $scope.closeThisDialog();
+                    if (keypadNumber < $scope.totalKeypad) {
+                        console.log(keypadNumber);
+                        $scope.configStepTwo(keypadNumber+1);
+                    }
+                }
+            }]
+        });
+    };
 
-.controller('DIYCtrl', function($scope, $ionicTabsDelegate, SweetAlert) {
-    SweetAlert.swal({
-       title: "How many keypads do you need?",
-       html: true,
-       type: "input",
-       inputType: "number",
-       inputPlaceholder: "Input a number, from 1 to 11",
-       showCancelButton: true,
-       confirmButtonColor: "#DD6B55",
-       confirmButtonText: "Next",
-       closeOnConfirm: false},
-        function(inputValue){
-            $scope.totalNumber = inputValue;
-            SweetAlert.swal("Nice!", "You wrote: " + inputValue, "success");
-        })
+    $scope.configStepTwoModify = function(index) {
+        ngDialog.open({
+            template: 'stepTwo',
+            closeByDocument: false,
+            scope: $scope,
+            controller: ['$scope', function($scope) {
+                $scope.keypadName = keypadName[index];
+                $scope.confirm = function(valName, val) {
+                    $scope.$parent.keypadsTypes[index].type = val;
+                    $scope.closeThisDialog();
+                }
+            }]
+        });
+    }
+
+
+    $scope.configStepOne = function() {
+            ngDialog.open({
+                template: 'stepOne',
+                closeByDocument: false,
+                scope: $scope,
+                controller: ['$scope', function($scope) {
+                    $scope.confirm = function(valName, val) {
+                        $scope.$parent[valName] = val;
+                        $scope.closeThisDialog();
+                        $scope.configStepTwo(1);
+                    }
+                }]
+            });
+    }
+    $scope.configStepOne();
+    
 })
