8d11a6543c3ba20107f52a03a77b5972f9764894
Dexter Yan <yanshaocong@gmail.com>
2015-09-19 16:13:14 +0800

    solve multi touch problem

--- www/js/controllers/DashController.js
+++ www/js/controllers/DashController.js
@@ -2,6 +2,9 @@ angular.module('dash.controller', ['starter.services'])
 
 
 .controller('DashCtrl', function($rootScope, $scope, socket) {
+    if ( $rootScope.currentKeypadType === '10b') {
+        $scope.tenKeypad = $rootScope.keypad[$rootScope.currentKeypad];
+    }
     $scope.addMsg = function(data) {
         socket.connect()
     }
@@ -17,22 +20,34 @@ angular.module('dash.controller', ['starter.services'])
             },
             link: function(scope, element, attrs) {
                 
-                // element.on('click', function() {
+                // var a = function() {
                 //     scope.info.led[0] = scope.info.led[0]=='on'?'off':'on';
                 //     scope.$apply();
-                // });
-                
-                scope.onTouch = function() {
+                // };
+
+                var onTouch = function() {
                     var sendData;
                     sendData = keypad.keypadNumberPrefix[0] + scope.info.name.toUpperCase();
                     socket.send(sendData);
                 };
 
-                scope.onRelease =  function() {
+                var onRelease =  function() {
                     var sendData;
                     sendData = keypad.keypadNumberPrefix[0] + scope.info.name.toLowerCase();
                    socket.send(sendData);
                 };
+                
+                element.on('touchstart', function() {
+                    scope.$apply(function() {
+                        scope.$eval(onTouch);
+                    });
+                });
+
+                element.on('touchend', function() {
+                    scope.$apply(function() {
+                        scope.$eval(onRelease);
+                    });
+                });
             }
         }
     }
