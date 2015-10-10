03887ed9f87edba0c48355484f9682c01ff559e3
anthonywlv <wlvanthony@gmail.com>
2015-10-10 07:01:24 +0000

    Add the linkage between buttons and keypad

--- www/js/controllers/DashController.js
+++ www/js/controllers/DashController.js
@@ -6,6 +6,7 @@ angular.module('dash.controller', ['starter.services'])
     $scope.currentKeypadType = $rootScope.currentKeypadType;
     $scope.maxKeypad = $rootScope.config.keypads.length;
     console.log($scope.maxKeypad);
+    
     $scope.addMsg = function(data) {
         socket.connect()
     };
@@ -36,10 +37,11 @@ angular.module('dash.controller', ['starter.services'])
         $scope.keypad = $rootScope.keypad[$rootScope.currentKeypad];
         console.log($scope.currentKeypadType);
     };
-
-    $scope.addMsg = function(data) {
-        socket.connect()
+    
+    $scope.resetName = function(data){
+        
     }
+    
 }])
 .directive('fourButtonKeypad', function(){
     return {
@@ -62,7 +64,10 @@ angular.module('dash.controller', ['starter.services'])
 .directive('forteenButtonKeypad', function(){
     return {
         restrict: 'E',
-        templateUrl: 'templates/elements/forteenButtonKeypad.html'
+        templateUrl: 'templates/elements/forteenButtonKeypad.html',
+        scope: {
+            keypad: '='
+        }
     }
 })
 .directive('keypadButton', function(keypad, socket, $ionicScrollDelegate) {
@@ -73,34 +78,34 @@ angular.module('dash.controller', ['starter.services'])
             info: '='
         },
         link: function(scope, element, attrs) {
-            // element.on('click', function() {
-            //     scope.info.led[0] = scope.info.led[0]=='on'?'off':'on';
-            //     scope.$apply();
+            element.on('click', function() {
+                scope.info.led[0] = scope.info.led[0]=='on'?'off':'on';
+                scope.$apply();
+            });
+
+            // var onTouch = function() {
+            //     var sendData;
+            //     sendData = keypad.keypadNumberPrefix[0] + scope.info.name.toUpperCase();
+            //     socket.send(sendData);
+            // };
+
+            // var onRelease =  function() {
+            //     var sendData;
+            //     sendData = keypad.keypadNumberPrefix[0] + scope.info.name.toLowerCase();
+            //   socket.send(sendData);
+            // };
+            
+            // element.on('touchstart', function() {
+            //     scope.$apply(function() {
+            //         scope.$eval(onTouch);
+            //     });
             // });
 
-            var onTouch = function() {
-                var sendData;
-                sendData = keypad.keypadNumberPrefix[0] + scope.info.name.toUpperCase();
-                socket.send(sendData);
-            };
-
-            var onRelease =  function() {
-                var sendData;
-                sendData = keypad.keypadNumberPrefix[0] + scope.info.name.toLowerCase();
-              socket.send(sendData);
-            };
-            
-            element.on('touchstart', function() {
-                scope.$apply(function() {
-                    scope.$eval(onTouch);
-                });
-            });
-
-            element.on('touchend', function() {
-                scope.$apply(function() {
-                    scope.$eval(onRelease);
-                });
-            });
+            // element.on('touchend', function() {
+            //     scope.$apply(function() {
+            //         scope.$eval(onRelease);
+            //     });
+            // });
         }
     }
 });
