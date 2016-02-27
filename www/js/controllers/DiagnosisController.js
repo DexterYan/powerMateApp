1e56f6b0e019da037e41ec15f1eaee0271273f6a
anthonywlv <wlvanthony@gmail.com>
2016-02-27 09:45:20 +0000

    Add connect button, move the button set to footer, turn off debug mode when leaving the debug page

--- www/js/controllers/DiagnosisController.js
+++ www/js/controllers/DiagnosisController.js
@@ -1,23 +1,25 @@
 angular.module('diagnosis.controller', ['starter.services'])
 
 .controller('DiagnosisCtrl', function($rootScope, $scope, socket){
-    $rootScope.enableDebug = true;
     $scope.diagnosisText = $rootScope.debugMsg;
     
     $scope.zx31 = function() {
         socket.sendHex([0x31, 0x80, 0x11,0x01]);
-    }
+    };
     
     $scope.zx30 = function() {
         socket.sendHex([0x31, 0x80, 0x11,0x00]);
-    }
+    };
     
-    $scope.reset = function() {
-        $localstorage.setObject('keypads', []);
-        $rootScope.config = {keypads: []};
-        $scope.keypadsTypes = [];
-        $scope.data.totalKeypad = null;
-        $ionicTabsDelegate.select(0);
-        $scope.configStepOne();
-    }
+    $scope.addMsg = function() {
+        socket.connect();
+    };
+    
+    $scope.$on('$ionicView.enter', function(){ //This is fired twice in a row
+        $rootScope.enableDebug = true;
+    });
+
+    $scope.$on('$ionicView.leave', function(){ //This just one when leaving, which happens when I logout
+        $rootScope.enableDebug = false;
+    });
 });
