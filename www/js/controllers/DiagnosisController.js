20852f7fb9529ed882c6f75d6176c0957de9cb67
Dexter.Yan <yanshaocong@gmail.com>
2016-05-17 18:09:39 +0800

    add input msg

--- www/js/controllers/DiagnosisController.js
+++ www/js/controllers/DiagnosisController.js
@@ -2,19 +2,29 @@ angular.module('diagnosis.controller', ['starter.services'])
 
 .controller('DiagnosisCtrl', function($rootScope, $scope, socket){
     $scope.diagnosisText = $rootScope.debugMsg;
-    
+    $scope.debugMsg = "";
+    $scope.zQuery = function() {
+        socket.send('z?');
+    };
+
     $scope.zx31 = function() {
         socket.sendHex([0x31, 0x80, 0x11,0x01]);
     };
-    
+
     $scope.zx30 = function() {
         socket.sendHex([0x31, 0x80, 0x11,0x00]);
     };
-    
+
     $scope.addMsg = function() {
         socket.connect();
     };
-    
+
+    $scope.sendMsg = function (msg) {
+        if (msg) {
+            socket.send(msg);
+        }
+    };
+
     $scope.$on('$ionicView.enter', function(){ //This is fired twice in a row
         $rootScope.enableDebug = true;
     });
